8332df2839c05002aa6a6c99c6c7f438ac50515c
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-25 20:05:35 -0700

    Updates to hour block styling

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ $("#currentDay").text(today);
 // $(".list-group-item").prepend('hello');
 
 function addTimeBlocks () {
-    const workdayHours = [7, 8, 9 ,10, 11, 12, 1, 2 ,3 ,4, 5, 6, 7, 8];
+    const workdayHours = ['7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM'];
     for (let i = 0; i < workdayHours.length; i++) {
-        let timeTag = '<div class="input-group-prepend"><span class="input-group-text bg-light" id="inputGroup-sizing-lg">Time</span></div>';
+        let timeTag = '<div class="input-group-prepend"><span class="input-group-text bg-light" id="inputGroup-sizing-lg">' + workdayHours[i] + '</span></div>';
         let inputText = "<input type='text' class='form-control bg-info' placeholder='Recipient's username' aria-label='Recipient's username' aria-describedby='button-addon2'>";
         let saveButton = '<div class="input-group-append"><button class="btn btn-outline-secondary bg-success" type="button" id="button-addon2">Button</button></div>';
-        let hourBlock = '<div class="input-group mb-3 input-group-lg d-flex p-2 bd-highlight bg-gradient-info">' + timeTag + inputText + saveButton + '</div>';
+        let hourBlock = '<div class="input-group input-group-lg d-flex bd-highlight bg-gradient-info">' + timeTag + inputText + saveButton + '</div>';
         
         calendarContainer.append(hourBlock);
         
