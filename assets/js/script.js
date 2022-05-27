b0fc17474d802a52f6c6be292a237cc1a281cd77
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-27 13:24:58 -0700

    updated time block logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,10 +55,19 @@ function colorEvents() {
         } else if (currentHour < eventTime && eventTime === 12 && currentTimeStr[1] === 'PM' && timeString[1] === 'PM') {
             updateBlock.eq(i).addClass('hour-passed');
             updateBlock.eq(i).attr("placeholder", "Event has already happened");
+        } else if (currentHour < eventTime && eventTime === 12 && currentTimeStr[1] === 'AM' && timeString[1] === 'PM') {
+            updateBlock.eq(i).addClass('future-hour');
         } else if (currentHour < eventTime && currentTimeStr[1] === 'PM' && timeString[1] === 'AM') {
             updateBlock.eq(i).addClass('hour-passed');
             updateBlock.eq(i).attr("placeholder", "Event has already happened");
         } else if (currentHour < eventTime && currentTimeStr[1] === 'PM' && timeString[1] === 'PM') {
+            updateBlock.eq(i).addClass('future-hour');
+        } else if (currentHour > eventTime && currentTimeStr[1] === 'AM' && timeString[1] === 'AM') {
+            updateBlock.eq(i).addClass('hour-passed');
+            updateBlock.eq(i).attr("placeholder", "Event has already happened");
+        } else if (currentHour < eventTime && currentTimeStr[1] === 'AM' && timeString[1] === 'AM') {
+            updateBlock.eq(i).addClass('future-hour');
+        } else if (currentHour > eventTime && currentTimeStr[1] === 'AM' && timeString[1] === 'PM') {
             updateBlock.eq(i).addClass('future-hour');
         }
     }
