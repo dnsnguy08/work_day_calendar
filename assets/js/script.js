a481af5088f2247c74b3f31602e3545d5b1ed2ea
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-26 22:26:52 -0700

    Added test steps for grabbing values from localStorage and init to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
+// Select all required elements and assign to variables
 const calendarContainer = $(".container");
 const today = moment().format('dddd, MMMM Do'); // assign today's date to a variable
 const now = moment().format('h.mmss A'); // the current time
 const saveBtn = $(".btn");
 const inputText = $(".form-control");
 
-const todos = [];
-
 $("#currentDay").text(today); // Assign today's date to the currentDay ID
 
 // Function for adding time blocks to the calendar page
 function addTimeBlocks () {
-    const workdayHours = ['7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM'];
+    const workdayHours = ['7-AM', '8-AM', '9-AM', '10-AM', '11-AM', '12-PM', '1-PM', '2-PM', '3-PM', '4-PM', '5-PM', '6-PM', '7-PM', '8-PM'];
     
     // Add hourBlock divs to the page based off of the workdayHours array
     for (let i = 0; i < workdayHours.length; i++) {
         let timeTag = '<div class="input-group-prepend"><span class="input-group-text bg-light" id="inputGroup-sizing-lg">' + workdayHours[i] + '</span></div>';
-        let inputText = "<input type='text' class='form-control events' placeholder='' aria-label='todo' aria-describedby='button-addon2'>";
+        let inputText = "<textarea type='text' class='form-control events' id='w" + workdayHours[i] + "' placeholder='' aria-label='todo' aria-describedby='button-addon2'/>";
         let saveButton = '<div class="input-group-append"><button class="btn btn-outline-secondary bg-info button-color" type="button" id="button-addon2">Save</button></div>';
         let hourBlock = '<div class="input-group input-group-lg d-flex bd-highlight bg-gradient-info">' + timeTag + inputText + saveButton + '</div>';
         
         calendarContainer.append(hourBlock);
         
     }
+
 }
 
 addTimeBlocks();
@@ -34,7 +34,7 @@ function colorEvents() {
 
     for (let i = 0; i < hourTag.length; i++) {
         // extract the time value from the hourTag HTML element and assign to a variable
-        let timeString = hourTag[i].innerHTML.split(" ");
+        let timeString = hourTag[i].innerHTML.split("-");
         let eventTime = parseInt(timeString[0]); // The time value from HTML element as an int
         
         // extract the hour from the current time string and assign to a variable
@@ -61,63 +61,53 @@ function colorEvents() {
 
 colorEvents();
 
+// Function for grabbing all values in localStorage and assigning to element IDs
+function init() {
+    console.log(localStorage.getItem("w7-AM"));
 
-$(".btn").on("click", function(){
+    $("#w7-AM").val(JSON.parse(localStorage.getItem("w7-AM")));
+
+    $("#w8-AM").val(JSON.parse(localStorage.getItem("w8-AM")));
+    
+    $("#w9-AM").val(JSON.parse(localStorage.getItem("w9-AM")));
+    
+    $("#w10-AM").val(JSON.parse(localStorage.getItem("w10-AM")));
+    
+    $("#w11-AM").val(JSON.parse(localStorage.getItem("w11-AM")));
+    
+    $("#w12-PM").val(JSON.parse(localStorage.getItem("w12-PM")));
+    
+    $("#w1-PM").val(JSON.parse(localStorage.getItem("w1-PM")));
+    
+    $("#w2-PM").val(JSON.parse(localStorage.getItem("w2-PM")));
+    
+    $("#w3-PM").val(JSON.parse(localStorage.getItem("w3-PM")));
+   
+    $("#w4-PM").val(JSON.parse(localStorage.getItem("w4-PM")));
+    
+    $("#w5-PM").val(JSON.parse(localStorage.getItem("w5-PM")));
+    
+    $("#w6-PM").val(JSON.parse(localStorage.getItem("w6-PM")));
+    
+    $("#w7-PM").val(JSON.parse(localStorage.getItem("w7-PM")));
+    
+    $("#w8-PM").val(JSON.parse(localStorage.getItem("w8-PM")));
+}
+
+$(".btn").on("click", function(){ // Save input value upon clicking the `Save` button 
     let userInput = $(this).parent('.input-group-append').siblings(".form-control").val();
-    todos.push(userInput);
-    console.log(userInput);
-    localStorage.setItem('todos', JSON.stringify(todos));
+    let hourSpan = 'w' + $(this).parent('.input-group-append').siblings(".input-group-prepend").children('span').text()
+
+    localStorage.setItem(hourSpan, JSON.stringify(userInput));
 });
 
-// function renderTodos() {
-//     // Clear todoList element and update todoCountSpan
-//     inputText.innerHTML = "";
-//     todoCountSpan.textContent = todos.length;
-  
-//     // Render a new li for each todo
-//     for (var i = 0; i < todos.length; i++) {
-//       var todo = todos[i];
-  
-//       var li = document.createElement("li");
-//       li.textContent = todo;
-//       li.setAttribute("data-index", i);
-  
-//       var button = document.createElement("button");
-//       button.textContent = "Complete ✔️";
-  
-//       li.appendChild(button);
-//       todoList.appendChild(li);
-//     }
-//   }
+// Clear all calendar events when 'Clear Calendar' button is pressed
+$("#clearCal").on("click", function(){
+    localStorage.clear();
+    init()
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).on('click','.btn', function(event) {
-//     event.preventDefault();
-//     // console.log('wwwwww');
-//     var todo = $(".form-control").val();
-//     console.log(todo);
-//     // var todo = {
-//     //     hour: $(document).val(hourTag),
-//     //     task: inputText.value
-//     // };
-//     // localStorage.setItem('todos', 'hey');
-
-// });
+// Init the page with todos saved in localStorage
+$(document).ready(function () {
+    init();
+});
