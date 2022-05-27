8713e34c87ea46254bfa7849296476d83f4583e4
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-27 15:04:59 -0700

    added condition for time block styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,6 +69,9 @@ function colorEvents() {
             updateBlock.eq(i).addClass('future-hour');
         } else if (currentHour > eventTime && currentTimeStr[1] === 'AM' && timeString[1] === 'PM') {
             updateBlock.eq(i).addClass('future-hour');
+        } else if (currentHour > eventTime && currentTimeStr[1] === 'PM' && timeString[1] === 'PM') {
+            updateBlock.eq(i).addClass('hour-passed');
+            updateBlock.eq(i).attr("placeholder", "Event has already happened");
         }
     }
 }
